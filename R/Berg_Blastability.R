54d05911a0f7a200ff6eb9aa96a199ce80e1aa59
Duah-Philip <valprogeny@gmail.com>
2023-05-02 09:20:55 -0500

    second commit

---
+++ R/Berg_Blastability.R
@@ -0,0 +1,19 @@
+
+
+#' Title
+#'
+#' @param RMD
+#' @param JPS
+#' @param JPA
+#' @param RDI
+#' @param HF
+#'
+#' @return
+#' @export
+#'
+#' @examples
+Berg_Blastability <-  function(RMD, JPS, JPA, RDI, HF ){
+  BI <-  0.5*(RMD+JPS+JPA+RDI+HF)
+  return(BI)
+}
+
